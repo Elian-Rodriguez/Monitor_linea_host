ba15af5661d8fe9f0693552e8840c22a432182c7
Elian-Rodriguez <elian.rodriguez@campusucc.edu.co>
2021-09-22 06:37:23 -0500

    VISUALISAR HISTORIAL
    SE CREA UN HIPERVINCULO PARA ACCEDER AL HISTORICO DE LINEA

--- Python/Tienda.py
+++ Python/Tienda.py
@@ -1,5 +1,6 @@
 #ESTE ES EL MODELO
 from app import mysql
+mysqls=mysql
 
 
 def select_all():
@@ -11,13 +12,13 @@ def select_all():
     return data
 
 def buscar_nombre_tienda(variable):
-     cur = mysql.connection.cursor()
-     alfa=variable
-     sentens = ('SELECT * FROM TIENDAS.Tienda where Tienda.NOMBRE like"%'+alfa+'%";')
-     cur.execute(sentens)
-     data = cur.fetchall()
-     return data
- 
+    cur = mysql.connection.cursor()
+    alfa=variable
+    sentens = ('SELECT * FROM TIENDAS.Tienda where Tienda.NOMBRE like"%'+alfa+'%";')
+    cur.execute(sentens)
+    data = cur.fetchall()
+    return data
+
 def listar_tiendas():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM Tienda ;")
@@ -34,20 +35,20 @@ def agregar_tienda(CODIGONCR, NOMBRETIENDA, CODIGOSAP, IPSERVER, IPPC, IPCAMARAS
     cur = mysql.connection.cursor()
     print(CODIGONCR, NOMBRETIENDA, CODIGOSAP, IPSERVER, IPPC, IPCAMARAS, ABIERT, ESTAD)
     cur.execute('INSERT INTO TIENDAS.Tienda (Cod_ncr, NOMBRE, CODIGO_SAP, IP_SERVER, IP_PC, IP_CAMARAS, Abierta, Estado) VALUES(%s, %s, %s, %s, %s, %s, %s, %s ); ',
-    (CODIGONCR, NOMBRETIENDA, CODIGOSAP, IPSERVER, IPPC, IPCAMARAS, ABIERT, ESTAD))
+                    (CODIGONCR, NOMBRETIENDA, CODIGOSAP, IPSERVER, IPPC, IPCAMARAS, ABIERT, ESTAD))
     mysql.connection.commit()
-    
+
 def actualizar_tienda(IPSERVER, IPPC, IPCAMARAS, ABIERT, ESTAD, CODIGONCR):
     cur = mysql.connection.cursor()
     cur.execute("""
-UPDATE TIENDAS.Tienda
-    SET IP_SERVER = %s, 
-    IP_PC =  %s, 
-    IP_CAMARAS=  %s, 
-    Abierta =  %s, 
-    Estado =  %s 
-WHERE (Cod_ncr = %s);       
-""", (IPSERVER, IPPC, IPCAMARAS, ABIERT, ESTAD, CODIGONCR))
+        UPDATE TIENDAS.Tienda
+            SET IP_SERVER = %s,
+            IP_PC =  %s,
+            IP_CAMARAS=  %s,
+            Abierta =  %s,
+            Estado =  %s
+        WHERE (Cod_ncr = %s);
+        """, (IPSERVER, IPPC, IPCAMARAS, ABIERT, ESTAD, CODIGONCR))
     mysql.connection.commit()
 
 
@@ -60,5 +61,26 @@ def mostrar_tienda(CODNCR):
 def eliminar_tienda(CODNCR):
     cur = mysql.connection.cursor()
     cur.execute(
-'DELETE FROM TIENDAS.Tienda WHERE (`Cod_ncr` = {0});'.format(CODNCR))
+        'DELETE FROM TIENDAS.Tienda WHERE (`Cod_ncr` = {0});'.format(CODNCR))
     mysql.connection.commit()
+
+def listar_historial_cod_ncr(CODNCR):
+    cur = mysqls.connection.cursor()
+    "call TIENDAS.LISTAR_HISTORIAL_COD_NCR(572)"
+    SQL = "call TIENDAS.LISTAR_HISTORIAL_COD_NCR("+CODNCR+")"
+    cur.execute(SQL)
+    dat = cur.fetchall()
+    return dat
+def listar_historial():
+    cur = mysql.connection.cursor()
+    sql ="call TIENDAS.LISTAR_HISTORIAL();"
+    cur.execute(sql)
+    dat = cur.fetchall()
+    return dat
+
+def listar_pos_server_caido():
+    cur = mysql.connection.cursor()
+    sql ="call TIENDAS.LISTAR_POS_SERVER_CAIDO();"
+    cur.execute(sql)
+    dat = cur.fetchall()
+    return dat
--- Python/app.py
+++ Python/app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
-import Tienda 
+import Tienda
 app = Flask(__name__)
 
 
@@ -22,7 +22,7 @@ def index():
     data=Tienda.select_all()
     return render_template('index.html', shops=data)
 
-#CARGUE DE LA PAGINA DE LOGIN DE ADMINISTRACION 
+#CARGUE DE LA PAGINA DE LOGIN DE ADMINISTRACION
 @app.route('/login')
 def login():
     return render_template('login.html')
@@ -32,59 +32,60 @@ def login():
 @app.route('/search', methods=['POST'])
 def search():
     if request.method == 'POST':
-buscar = request.form['Buscador']
-data=Tienda.buscar_nombre_tienda(buscar) 
+        buscar = request.form['Buscador']
+        data=Tienda.buscar_nombre_tienda(buscar)
     return render_template('Search.html', shops=data)
 
 #INDEX PARA ADMINISTRADOR  O PAGIANA DE ADMINISTRACION
 @app.route('/index_admin',  methods=['POST'])
 def index_admin():
     if request.method == 'POST':
-usuario = request.form['user']
-password = request.form['pass']
-print("usuario : " + usuario)
-print("clave : " + password)
-if (usuario == "Homero" and password == "Linux-2020"):
-    data = Tienda.listar_tiendas()
-    data_2=Tienda.listar_offline()
-    menssanges = 'Ingreso Autorizado'
-    flash(menssanges)
-    return render_template('index_admin.html', shops=data, offlines=data_2)
-else:
-    menssanges = 'Ingreso Incorrecto, Intente Nuevamente'
-    flash(menssanges)
-    return redirect(url_for('login'))
+        usuario = request.form['user']
+        password = request.form['pass']
+        print("usuario : " + usuario)
+        print("clave : " + password)
+        if (usuario == "Homero" and password == "Linux-2020"):
+            data = Tienda.listar_tiendas()
+            data_2=Tienda.listar_offline()
+            data_3=Tienda.listar_pos_server_caido()
+            menssanges = 'Ingreso Autorizado'
+            flash(menssanges)
+            return render_template('index_admin.html', shops=data, offlines=data_2, CAIDA_POS_SERVER =data_3)
+        else:
+            menssanges = 'Ingreso Incorrecto, Intente Nuevamente'
+            flash(menssanges)
+            return redirect(url_for('login'))
 
 #MENU DE CREACION DE TIENDA NUEVA
 @app.route('/add_shop', methods=['POST'])
 def add_shop():
     if request.method == 'POST':
-CODIGO_NCR = request.form['CODIGO_NCR']
-NOMBRE_TIENDA = request.form['NOMBRE_TIENDA']
-CODIGO_SAP = request.form['CODIGO_SAP']
-IP_SERVER = request.form['IP_SERVER']
-IP_PC = request.form['IP_PC']
-IP_CAMARAS = request.form['IP_CAMARAS']
-ESTADO = request.form['ESTADO']
-ABIERTA = request.form['ABIERTA']
-Tienda.agregar_tienda(CODIGO_NCR, NOMBRE_TIENDA, CODIGO_SAP, IP_SERVER, IP_PC, IP_CAMARAS, ABIERTA, ESTADO)
-menssanges = 'Store Added Satisfactorily'
-flash(menssanges)
+        CODIGO_NCR = request.form['CODIGO_NCR']
+        NOMBRE_TIENDA = request.form['NOMBRE_TIENDA']
+        CODIGO_SAP = request.form['CODIGO_SAP']
+        IP_SERVER = request.form['IP_SERVER']
+        IP_PC = request.form['IP_PC']
+        IP_CAMARAS = request.form['IP_CAMARAS']
+        ESTADO = request.form['ESTADO']
+        ABIERTA = request.form['ABIERTA']
+        Tienda.agregar_tienda(CODIGO_NCR, NOMBRE_TIENDA, CODIGO_SAP, IP_SERVER, IP_PC, IP_CAMARAS, ABIERTA, ESTADO)
+        menssanges = 'Store Added Satisfactorily'
+        flash(menssanges)
     return redirect(url_for('index'))
 
 #RETORNO DE BUSQUEDA DE TIENDA Y MENU DE EDICION
 @app.route('/update_shop/<string:CODIGO_NCR>', methods=['POST'])
 def update_shop(CODIGO_NCR):
     if request.method == 'POST':
-IP_SERVER = request.form['IP_SERVER']
-IP_PC = request.form['IP_PC']
-IP_CAMARAS = request.form['IP_CAMARAS']
-ESTADO = request.form['ESTADO']
-ABIERTA = request.form['ABIERTA']
-Tienda.actualizar_tienda(IP_SERVER, IP_PC, IP_CAMARAS, ABIERTA, ESTADO, CODIGO_NCR)
-menssanges = 'Store Update Satisfactorily'
-flash(menssanges)
-return redirect(url_for('index'))
+        IP_SERVER = request.form['IP_SERVER']
+        IP_PC = request.form['IP_PC']
+        IP_CAMARAS = request.form['IP_CAMARAS']
+        ESTADO = request.form['ESTADO']
+        ABIERTA = request.form['ABIERTA']
+        Tienda.actualizar_tienda(IP_SERVER, IP_PC, IP_CAMARAS, ABIERTA, ESTADO, CODIGO_NCR)
+        menssanges = 'Store Update Satisfactorily'
+        flash(menssanges)
+        return redirect(url_for('index'))
 
 #ACTUALIZACION DE TIENSA SOLO PERMITE IP Y ESTADO
 @app.route('/edit_shop/<string:CODIGO_NCR>')
@@ -95,16 +96,20 @@ def get_shop(CODIGO_NCR):
     flash(menssanges)
     return render_template('edit-shop.html', shop=data[0])
 
-#ELIMINAR TIENDA 
+#ELIMINAR TIENDA
 @app.route('/delete_shop/<string:CODIGO_NCR>')
 def delete_shop(CODIGO_NCR):
     Tienda.eliminar_tienda(CODIGO_NCR)
     menssanges = 'Store Removed Satisfactorily'
     flash(menssanges)
     return redirect(url_for('index'))
+@app.route('/listar_historial')
+def listar_historial():
+    data = Tienda.listar_historial()
+    return render_template('listar_historial.html',historial=data)
 
 
 if __name__ == '__main__':
-    app.run(port=3000, host="0.0.0.0", debug=True)
+    app.run(port=3030, host="0.0.0.0", debug=True)
     #app.run(port=3030, host="0.0.0.0", debug=True)
 
